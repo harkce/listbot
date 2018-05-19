51f1b77f25acf87f64162d4e753c9bffc31e002c
Habib Fikri <habibgarut@gmail.com>
2018-05-20 06:12:47 +0700

    Raw command

--- webhook/handler.go
+++ webhook/handler.go
@@ -3,6 +3,7 @@ package webhook
 import (
 	"log"
 	"net/http"
+	"strings"
 
 	"github.com/harkce/listbot"
 	"github.com/harkce/listbot/server/response"
@@ -17,6 +18,10 @@ func hookResp(w http.ResponseWriter) {
 	response.OK(w, resp)
 }
 
+func unsupportedEvent(e *linebot.Event) bool {
+	return e.Type != linebot.EventTypeMessage || e.Source != linebot.EventSourceTypeGroup
+}
+
 func (h *Handler) WebHook(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	events, err := listbot.Client.ParseRequest(r)
 	if err != nil {
@@ -26,9 +31,34 @@ func (h *Handler) WebHook(w http.ResponseWriter, r *http.Request, _ httprouter.P
 	}
 
 	for _, event := range events {
-		if event.Type != linebot.EventTypeMessage {
-			hookResp(w)
-			return
+		if unsupportedEvent(event) {
+			continue
+		}
+
+		message, ok := event.Message.(*linebot.TextMessage)
+		if !ok {
+			continue
+		}
+
+		content := message.Text
+		if !strings.HasPrefix(content, "/") {
+			continue
+		}
+
+		if strings.HasPrefix(content, "/list") {
+			continue
+		}
+		if strings.HasPrefix(content, "/title") {
+			continue
+		}
+		if strings.HasPrefix(content, "/add") {
+			continue
+		}
+		if strings.HasPrefix(content, "/delete") {
+			continue
+		}
+		if strings.HasPrefix(content, "/clear") {
+			continue
 		}
 	}
 
