06a7379043bd0d854565d0d357d71e7096ab0b2f
Habib Fikri <habibgarut@gmail.com>
2018-05-20 17:19:55 +0700

    .env

--- listbot/linebot.go
+++ listbot/linebot.go
@@ -10,7 +10,7 @@ var Client *linebot.Client
 
 func InitBot() error {
 	channelSecret := os.Getenv("CHANNEL_SECRET")
-	channelToken := os.Getenv("CHANNEL_TOKEN")
+	channelToken := os.Getenv("CHANNEL_TOKEN") + "="
 
 	var err error
 	if Client, err = linebot.New(channelSecret, channelToken); err != nil {
